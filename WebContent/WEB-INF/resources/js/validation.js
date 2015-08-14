d6a86ace21cf8e379965ba354e80407b37979be9
tamtran116 <thtt116@me.com>
2015-08-13 23:05:59 -0500

    add user egistering service (temp)

--- WebContent/WEB-INF/resources/js/validation.js
+++ WebContent/WEB-INF/resources/js/validation.js
@@ -17,12 +17,22 @@ function validateForm() {
     * $("#formFirstName") doesn't select the element directly, it selects a jQuery object which contains (among other things) the element,
     *  to access actual DOM properties you must select the first one.
     * */
-    var inpFirstName = $('#formFirstName').val;
-    if ($('#formFirstName').val.length = 0 || $("#formFirstName").val.length > 20 ) {
-        $('#formFirstName').parent().addClass("has-error");
-        alert("not valid due to input length = " + inpFirstName);
+    var successSpan =
+        '<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>' +
+        '<span id="inputSuccess2Status" class="sr-only">(success)</span>';
+    var errorSpan =
+        '<span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>' +
+        '<span id="inputWarning2Status" class="sr-only">(warning)</span>'
+    var inpFirstName = $('#formFirstName').val();
+    if (inpFirstName.length = 0 || inpFirstName.length > 20 ) {
+        $('#formFirstName').parent().removeClass();
+        $('#formFirstName').parent().addClass("form-group has-error has-feedback");
+        $('#formFirstName').parent().append(errorSpan);
+        alert("not valid due to input length = " + inpFirstName.length);
     } else {
-        $('#formFirstName').parent().addClass("has-success");
-        alert("valid due to input length = " + inpFirstName);
+        $('#formFirstName').parent().removeClass();
+        $('#formFirstName').parent().addClass("form-group has-success has-feedback");
+        $('#formFirstName').parent().append(successSpan);
+        alert("valid due to input length = " + inpFirstName.length);
     }
 }
