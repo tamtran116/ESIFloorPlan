5032b286bb5c47819e578e929d5c8aec67debcae
tamtran116 <thtt116@me.com>
2016-03-13 12:47:02 -0500

    enhance form validation

--- myreceipt-war/src/main/webapp/WEB-INF/resources/js/validation.js
+++ myreceipt-war/src/main/webapp/WEB-INF/resources/js/validation.js
@@ -10,78 +10,128 @@
     };
 })();
 
-/**
- * Created by Tam Tran on 8/2/2015.
- */
-var phoneRegex ='^(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?(?:\\(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\\s*\\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\\s*(?:[.-]\\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\\s*(?:[.-]\\s*)?([0-9]{4})(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+))?$';
-var socialRegex ='^\\d{3}-\\d{3}-\\d{4}$';
-var emailRegex = /^[-a-z0-9~!$%^&*_=+}{'?]+(\.[-a-z0-9~!$%^&*_=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-var alphaNumericRegex = /^[a-zA-Z0-9\s]*$/;
 $(document).ready(function() {
+    console.log( "document loaded" );
+
+    // regex taken from https://developers.google.com/web/fundamentals/design-and-ui/input/forms/provide-real-time-validation?hl=en
+    var phoneRegex ='^(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?(?:\\(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\\s*\\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\\s*(?:[.-]\\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\\s*(?:[.-]\\s*)?([0-9]{4})(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+))?$',
+        emailRegex = /^[-a-z0-9~!$%^&*_=+}{'?]+(\.[-a-z0-9~!$%^&*_=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i,
+        alphaNumericRegex = /^[a-zA-Z0-9\s]*$/;
+
+    // add popover for every form group class
+    $('.form-group').attr('data-toggle', 'popover');
+
+    // manually trigger popover
     var popOverOptions = {
         container : 'body',
         trigger: 'manual'
     };
-    console.log( "document loaded" );
+
+    // initialize bootstrap popover functionality
     $('[data-toggle="popover"]').popover(popOverOptions);
-    $("#formPhoneNumber").mask('(000) 000-0000');
-    $('#first-name-inp').validateFormInput("First Name", true, 0, 10, "^[a-zA-Z ]*$");
-    $('#last-name-inp').validateFormInput("Last Name", true, 0, 10, "^[a-zA-Z ]*$");
 
-    /*$('form-signin').on("keyup", function() {
-        validateRegex("#formPhoneNumber", phoneRegex);
-        validateRegex("#formEmail", emailRegex);
-        validateConfirm("#formConfirmedEmail", "#formEmail");
-    });*/
+    // init input jquery objects
+    var unInp = $('#user-name-inp'),
+        pwInp = $('#password-inp'),
+        fnInp = $('#first-name-inp'),
+        lnInp = $('#last-name-inp'),
+        phInp = $("#phone-number-inp"),
+        emInp = $('#email-inp'),
+        confEmInp = $('#email-confirm-inp');
 
+    fnInp.validateFormInput("First Name", true, 0, 10, "^[a-zA-Z ]*$");
+    lnInp.validateFormInput("Last Name", true, 0, 10, "^[a-zA-Z ]*$");
+    phInp.mask('(000) 000-0000').validateFormInput("Phone Number", true, 0, 14, phoneRegex);
+    emInp.validateFormInput("Email", true, 0, 50, emailRegex);
+    confEmInp.validateFormInput("Email Confirm",true, 0, 50, emailRegex, emInp);
+    unInp.validateFormInput("User Name", true, 0, 20, alphaNumericRegex);
+    //pwInp.validateFormInput("Password", true, 0, 0, null);
+    pwInp.strengthenPassword(6, true, true);
 });
-
-$.fn.validateFormInput = function (source, required, minLength, maxLength, pattern) {
+$.fn.strengthenPassword = function (minLength, specialChar, capitalChar) {
+    var warningList = $('<ul>', {
+        class:"list-unstyled"
+    });
     this.on('keyup', function (e) {
         var code = (e.keyCode || e.which);
-        // do nothing if it's an arrow key
+        // do nothing if it's an arrow key or tab
         if(code == 37 || code == 38 || code == 39 || code == 40 || code == 9) {
             return;
         }
-        var length = this.value.length;
-        var parent = this.parentElement;
-        var status = source.toLowerCase().replace(' ','-') + "-inp-status";
-        $(parent).toggleClass("has-error", false);
-        $(parent).toggleClass("has-success", false);
-        $(parent).toggleClass("has-feedback", false);
-        $(parent).find('span').remove();
+        var length = this.value.length,
+            capitalCharRegex = /[A-Z]/,
+            lowCapitalCharRegex = /[a-z]/,
+            $parent = $(this).parent();
 
-        if (!this.value) {
-            $(this).populateError("Missing required input " + source);
-        } else if (length > maxLength || length < minLength) {
-            $(this).populateError("Invalid length for " + source);
-        } else if (!new RegExp(pattern).test(this.value)) {
-            $(this).populateError("Invalid value for " + source);
-        } else {
-            $(this).populateSuccess();
+        warningList.empty();
+        if(minLength && length < minLength) {
+            warningList.append('<li style="color: #b6434f;"><span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span> Password is less than min length.</li>');
         }
+        if (capitalChar && !new RegExp(lowCapitalCharRegex).test(this.value)) {
+            warningList.append('<li style="color: #b6434f;"><span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span> Password does not contain a low cap character.</li>');
+        }
+        if (capitalChar && !new RegExp(capitalCharRegex).test(this.value)) {
+            warningList.append('<li style="color: #b6434f;"><span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span> Password does not contain a capital character.</li>');
+        }
+        //$parent.attr('data-content', warningList);
+        //$parent.popover('show');
+        $parent.append(warningList);
+        //console.log(list);
     });
 };
-$.fn.populateError = function (message) {
-    var $element = this;
-    var $parent = $element.parent();
-    $parent.attr('data-content', message);
-    $parent.popover('show');
-    $parent.toggleClass("has-error", true);
-    $parent.toggleClass("has-feedback", true);
-    $parent.append('<span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>');
-    $element.attr('aria-describedby', status);
-    $parent.append('<span id="' + status + '" class="sr-only">(warning)</span>');
+
+$.fn.validateFormInput = function (source, required, minLength, maxLength, pattern, targetInp) {
+    var consoleMessage;
+    this.on('focusout', function () {
+        console.log(consoleMessage);
+    });
+    this.on('keyup', function (e) {
+        var code = (e.keyCode || e.which);
+        // do nothing if it's an arrow key or tab
+        if(code == 37 || code == 38 || code == 39 || code == 40 || code == 9) {
+            return;
+        }
+        var valid = false,
+            inpValue = this.value,
+            length = inpValue.length,
+            status = source.toLowerCase().replace(' ','-') + "-inp-status";
+
+        if (required && !inpValue) {
+            consoleMessage = "Missing required input " + source;
+        } else if (targetInp && inpValue !== targetInp.val()) {
+            consoleMessage = source + " not matched its source.";
+        } else if (maxLength > 0 && length > maxLength) {
+            consoleMessage = source + " has more than " + (length - maxLength) + " characters allowed.";
+        } else if (minLength && length < minLength) {
+            consoleMessage = source + " needs more " + (minLength - length) + " characters.";
+        } else if (pattern && !new RegExp(pattern).test(inpValue)) {
+            consoleMessage = "Invalid value \"" + inpValue + "\" for " + source;
+        } else {
+            consoleMessage = source + " is valid.";
+            valid = true;
+        }
+        $(this).populatePopover(valid, consoleMessage);
+    });
 };
-$.fn.populateSuccess = function () {
+$.fn.populatePopover = function (valid, message) {
     var $element = this;
     var $parent = $element.parent();
-    $parent.popover('hide');
-    $parent.toggleClass("has-error", false);
+    $parent.toggleClass("has-error", false).toggleClass("has-success", false).toggleClass("has-feedback", false);
+    $parent.find('span').remove();
     $parent.toggleClass("has-feedback", true);
-    $parent.toggleClass("has-success", true);
-    $parent.append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
-    $element.attr('aria-describedby', status );
-    $parent.append('<span id="' + status + '" class="sr-only">(success)</span>');
+    $element.attr('aria-describedby', status);
+    if(valid) {
+        $parent.attr('data-content', "");
+        $parent.popover('hide');
+        $parent.toggleClass("has-success", true);
+        $parent.append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+        $parent.append('<span id="' + status + '" class="sr-only">(success)</span>');
+    } else {
+        $parent.attr('data-content', message);
+        $parent.popover('show');
+        $parent.toggleClass("has-error", true);
+        $parent.append('<span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>');
+        $parent.append('<span id="' + status + '" class="sr-only">(warning)</span>');
+    }
 };
 
