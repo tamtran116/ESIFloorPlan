47d10da335ab5d72e214a9deda33f271fb6b8b07
tamtran116 <thtt116@me.com>
2016-03-12 21:25:20 -0600

    enhance form validation

--- myreceipt-war/src/main/webapp/WEB-INF/resources/js/validation.js
+++ myreceipt-war/src/main/webapp/WEB-INF/resources/js/validation.js
@@ -1,4 +1,16 @@
 /**
+ * Check if have jquery. if not include
+ */
+(function() {
+    window.onload = function () {
+        if (!window.jQuery) {
+            alert("please include jquery");
+            throw new Error("Missing jquery");
+        }
+    };
+})();
+
+/**
  * Created by Tam Tran on 8/2/2015.
  */
 var phoneRegex ='^(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?(?:\\(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\\s*\\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\\s*(?:[.-]\\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\\s*(?:[.-]\\s*)?([0-9]{4})(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+))?$';
@@ -6,76 +18,70 @@ var socialRegex ='^\\d{3}-\\d{3}-\\d{4}$';
 var emailRegex = /^[-a-z0-9~!$%^&*_=+}{'?]+(\.[-a-z0-9~!$%^&*_=+}{'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
 var alphaNumericRegex = /^[a-zA-Z0-9\s]*$/;
 $(document).ready(function() {
+    var popOverOptions = {
+        container : 'body',
+        trigger: 'manual'
+    };
     console.log( "document loaded" );
+    $('[data-toggle="popover"]').popover(popOverOptions);
     $("#formPhoneNumber").mask('(000) 000-0000');
-    validateLength("#formFirstName", 20, 1);
-    validateLength("#formLastName", 20, 1);
-    $('form-signin').on("keyup", function() {
+    $('#first-name-inp').validateFormInput("First Name", true, 0, 10, "^[a-zA-Z ]*$");
+    $('#last-name-inp').validateFormInput("Last Name", true, 0, 10, "^[a-zA-Z ]*$");
+
+    /*$('form-signin').on("keyup", function() {
         validateRegex("#formPhoneNumber", phoneRegex);
         validateRegex("#formEmail", emailRegex);
         validateConfirm("#formConfirmedEmail", "#formEmail");
-    });
+    });*/
 
 });
 
-function validateLength(jquerySelector, maxLength, minLength) {
-    $(jquerySelector).on('keyup', function () {
-        var describeAttribute = jquerySelector.substring(5);
-        var valueLength = $(jquerySelector).val().length;
-        var parentDiv = $(jquerySelector).parent();
-        if (valueLength > maxLength || valueLength < minLength) {
-            errorMessage =
-                addError(parentDiv, jquerySelector, describeAttribute, "not valid due to input length = " + $(jquerySelector).val().length)
+$.fn.validateFormInput = function (source, required, minLength, maxLength, pattern) {
+    this.on('keyup', function (e) {
+        var code = (e.keyCode || e.which);
+        // do nothing if it's an arrow key
+        if(code == 37 || code == 38 || code == 39 || code == 40 || code == 9) {
+            return;
+        }
+        var length = this.value.length;
+        var parent = this.parentElement;
+        var status = source.toLowerCase().replace(' ','-') + "-inp-status";
+        $(parent).toggleClass("has-error", false);
+        $(parent).toggleClass("has-success", false);
+        $(parent).toggleClass("has-feedback", false);
+        $(parent).find('span').remove();
+
+        if (!this.value) {
+            $(this).populateError("Missing required input " + source);
+        } else if (length > maxLength || length < minLength) {
+            $(this).populateError("Invalid length for " + source);
+        } else if (!new RegExp(pattern).test(this.value)) {
+            $(this).populateError("Invalid value for " + source);
         } else {
-            addSuccess(parentDiv, jquerySelector, describeAttribute);
+            $(this).populateSuccess();
         }
     });
-}
+};
+$.fn.populateError = function (message) {
+    var $element = this;
+    var $parent = $element.parent();
+    $parent.attr('data-content', message);
+    $parent.popover('show');
+    $parent.toggleClass("has-error", true);
+    $parent.toggleClass("has-feedback", true);
+    $parent.append('<span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span>');
+    $element.attr('aria-describedby', status);
+    $parent.append('<span id="' + status + '" class="sr-only">(warning)</span>');
+};
+$.fn.populateSuccess = function () {
+    var $element = this;
+    var $parent = $element.parent();
+    $parent.popover('hide');
+    $parent.toggleClass("has-error", false);
+    $parent.toggleClass("has-feedback", true);
+    $parent.toggleClass("has-success", true);
+    $parent.append('<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>');
+    $element.attr('aria-describedby', status );
+    $parent.append('<span id="' + status + '" class="sr-only">(success)</span>');
+};
 
-function validateRegex(jquerySelector, pattern) {
-    $(jquerySelector).on('keyup', function () {
-        var describeAttribute = jquerySelector.substring(5);
-        var value = $(jquerySelector).val();
-        var regex = new RegExp(pattern);
-        var parentDiv = $(jquerySelector).parent();
-        if (regex.test(value)) {
-            addSuccess(parentDiv, jquerySelector, describeAttribute);
-        } else {
-            addError(parentDiv, jquerySelector, describeAttribute, "not valid format")
-        }
-    });
-}
-
-function validateConfirm(jquerySelector, jsConfirmElement) {
-    $(jquerySelector).on('keyup', function () {
-        var describeAttribute = jquerySelector.substring(5);
-        var objectToConfirm = $(jsConfirmElement).val();
-        var confirmObj = $(jquerySelector).val();
-        var parentDiv = $(jquerySelector).parent();
-
-        if (objectToConfirm == confirmObj) {
-            addSuccess(parentDiv, jquerySelector, describeAttribute);
-        } else {
-            addError(parentDiv, jquerySelector, describeAttribute, "fail confirm, value not match")
-
-        }
-    });
-}
-
-function addError(jqueryObj, jsSelector, descAttr, errorMsg) {
-    jqueryObj.find(".errorMessage").remove();
-    jqueryObj.removeClass();
-    jqueryObj.addClass("form-group has-error has-feedback");
-    jqueryObj.find(".glyphicon").removeClass().addClass("glyphicon glyphicon-remove form-control-feedback");
-    $(jsSelector).attr('aria-describedby','').attr('aria-describedby', descAttr+'Error2Status');
-    jqueryObj.append("<p class='errorMessage'>" + errorMsg + "</p>");
-}
-
-function addSuccess(jqueryObj, jsSelector, descAttr) {
-    jqueryObj.find(".errorMessage").remove();
-    jqueryObj.removeClass();
-    jqueryObj.addClass("form-group has-success has-feedback");
-    jqueryObj.find(".glyphicon").removeClass().addClass("glyphicon glyphicon-ok form-control-feedback");
-    $(jsSelector).attr('aria-describedby','').attr('aria-describedby', descAttr+'Success2Status');
-
-}
