12f37eb54e5c402f2e6ef5e12af87014d95d3cc2
tamtran116 <thtt116@me.com>
2016-03-20 22:19:39 -0500

    enhance ocr

--- myreceipt-business/src/main/java/com/tamtran/myreceipt/business/tesseract/TessOcr.java
+++ myreceipt-business/src/main/java/com/tamtran/myreceipt/business/tesseract/TessOcr.java
@@ -1,12 +1,28 @@
 package com.tamtran.myreceipt.business.tesseract;
 
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.MetadataException;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.jpeg.JpegDirectory;
 import com.tamtran.myreceipt.common.utils.OSValidator;
+import net.coobird.thumbnailator.Thumbnails;
 import org.bytedeco.javacpp.BytePointer;
 import org.bytedeco.javacpp.lept;
 import org.bytedeco.javacpp.tesseract.*;
+import org.w3c.dom.Node;
 
+import javax.imageio.ImageIO;
+import javax.imageio.metadata.IIOInvalidTreeException;
+import javax.imageio.metadata.IIOMetadata;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,6 +41,10 @@ public class TessOcr {
     public String processRaw(String file) throws Exception{
         TessBaseAPI api = new TessBaseAPI();
         api.SetVariable("tessedit_char_whitelist", "0123456789,/ABCDEFGHJKLMNPQRSTUVWXY");
+        api.SetPageSegMode(0);
+        System.out.println("PageSegMode = "+api.GetPageSegMode());
+        api.ClearAdaptiveClassifier();
+
         if (OSValidator.isWindows() && api.Init("C:/Tesseract-OCR" , "eng") != 0) {
             throw new Exception("Could not initialize tesseract.");
         } else if (OSValidator.isUnix() && api.Init("/usr/local/share/" , "eng") != 0){
@@ -32,12 +52,35 @@ public class TessOcr {
         }
         lept.PIX image = null;
         BytePointer outText = null;
+        int orientation = readImageInformation(new File(file));
         try {
             image = lept.pixRead(file);
+            switch (orientation) {
+                case 1: default: {
+                    //do nothing
+                    break;
+                }
+                case 8: {
+                    System.out.println("rotate img to -90 degrees");
+                    image = lept.pixRotate90(image, -1);
+                    break;
+                }
+                case 6:
+                {
+                    System.out.println("rotate img to 90 degrees");
+                    image = lept.pixRotate90(image, 1);
+                    break;
+                }
+                case 3:
+                {
+                    System.out.println("rotate img to 180 deg");
+                    image = lept.pixRotate180(null, image);
+                    break;
+                }
+            }
             api.SetImage(image);
             outText = api.GetUTF8Text();
             String string = outText.getString("UTF-8");
-//            String stringResult = string.replaceAll("[^\\w\n\\.\\*$;: ]","");
             if (string != null) {
                 string = string.trim();
             }
@@ -55,5 +98,28 @@ public class TessOcr {
                 api.End();
             }
         }
+    }
+
+    private int readImageInformation(File imageFile)  throws IOException, MetadataException, ImageProcessingException {
+        Metadata metadata = ImageMetadataReader.readMetadata(imageFile);
+        Directory directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
+//        JpegDirectory jpegDirectory = metadata.getFirstDirectoryOfType(JpegDirectory.class);
+
+        int orientation = 1;
+//        int width = 0;
+//        int height = 0;
+        try {
+            orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
+//            height = directory.getInt(ExifIFD0Directory.TAG_IMAGE_HEIGHT);
+//            width = directory.getInt(ExifIFD0Directory.TAG_IMAGE_WIDTH);
+        } catch (MetadataException me) {
+            System.out.println("Could not get orientation");
+        }
+//        int width = jpegDirectory.getImageWidth();
+//        int height = jpegDirectory.getImageHeight();
+
+        System.out.println("Image orientation = "+orientation);
+
+        return orientation;
     }
 }
